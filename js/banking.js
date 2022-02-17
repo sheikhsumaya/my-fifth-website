008b6b3ec657093db4e42912e684b77b5ad02e56
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-02-17 23:57:01 +0600

    forth

--- js/banking.js
+++ js/banking.js
@@ -1,58 +1,68 @@
-document.getElementById('calculate-button').addEventListener
-('click', function(){
-
-    //     //  income 
-
-    // const incomeInput = document.getElementById('income-input');
-    // const incomeInputText = incomeInput.innerText;
-    // const finalIncome = parseFloat(incomeInputText);
+function getTotalCalculation(incomeId, foodId, rentId, clothesId, totalInputId){
+    // income 
+    const incomeInputId = document.getElementById(incomeId);
+    const incomeInputText = incomeInputId.value;
+    const incomeInput = parseFloat(incomeInputText);
+    if(typeof(incomeInput) == 'number'){
+        alert("Enter Number");
+    }
+    else if(incomeInput < 0){
+        alert("Please enter a positive number!! ");
+    }
     
-        // food 
+    console.log(incomeInput);
+    incomeInputId.value = '';
 
-
-
-
-    const foodInput = document.getElementById('food-input');
+    // food
+    const foodInput = document.getElementById(foodId);
     const foodInputText = foodInput.value;
     const finalFood = parseFloat(foodInputText);
-    finalFood.value = foodInput;
+    if(typeof(finalFood) == 'number'){
+        alert("Enter Number");
+    }
+    else if(finalFood < 0){
+        alert("Please enter a positive number!! ");
+    }
     console.log(finalFood);
     foodInput.value = '';
 
-        // rent
-
-    const rentInput = document.getElementById('rent-input');
+    // rent
+    const rentInput = document.getElementById(rentId);
     const rentInputText = rentInput.value;
-    const finalRent = parseFloat(rentInputText);
-    finalRent.value = rentInput;
+    const finalRent = parseFloat (rentInputText);
+    if(typeof(finalRent) == 'number'){
+        alert("Enter Number");
+    }
+    else if(finalRent < 0){
+        alert("Please enter a positive number!! ");
+    }
     console.log(finalRent);
     rentInput.value = '';
 
-        // clothes
-     
-    const clothesInput = document.getElementById('clothes-input');
+    // clothes
+    const clothesInput = document.getElementById(clothesId)
     const clothesInputText = clothesInput.value;
-    const finalClothes = parseFloat(clothesInputText);
-    finalClothes.value = clothesInput;
-    console.log(finalClothes);
+    const finalclothes = parseFloat(clothesInputText);
+    if(typeof(finalclothes) == 'number'){
+        alert("Enter Number");
+    }
+    else if(finalclothes < 0){
+        alert("Please enter a positive number!! ");
+    }
+    console.log(finalclothes);
     clothesInput.value = '';
-
-        // total calculate 
-    const totalInput = document.getElementById('expenses-total');
-    const totalText = totalInput.innerText;
-    const newFinalTotal = parseFloat(totalText);
-    totalInput.innerText = newFinalTotal;
-    // newFinalTotal = foodInput + rentInput + clothesInput;
-    console.log(newFinalTotal);
-
-
     
+    // total
+    const totalInput = document.getElementById(totalInputId);
+    const totalText = totalInput.innerText;
+    if(incomeInput>0 && finalFood>0 && finalRent>0 && finalclothes>0){
+        const finalTotal = incomeInput - finalFood - finalRent - finalclothes;
+        totalInput.innerText = finalTotal;
+    }
+    console.log(finalTotal);
+}
 
-
+document.getElementById('calculate-button').addEventListener
+('click', function(){
+    getTotalCalculation('income-input', 'food-input', 'rent-input', 'clothes-input', 'expenses-total')
 })
-// const balanceTotal = document.getElementById('balance-total');
-// const balanceTotalText = balanceTotal.innerText;
-// const previousBalanceTotal = parseFloat(balanceTotalText);
-// const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-// balanceTotal.innerText = newBalanceTotal;
-
